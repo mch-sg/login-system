af7335e1d117ae80dd89e7bbc54ead2f1504860c
mch-sg <magnus.hvidtfeldt@gmail.com>
2022-12-26 12:24:46 +0100

    php + xampp init

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <title>Login System Test</title>
+</head>
+<body>
+
+<nav>  
+    <div class="wrapper">
+        <a href="index.php"><img src="img/logo-white.png" alt=""></a>
+        <ul>
+            <li><a href="index.php">Projects</a></li>
+            <li><a href="index.php">Settings</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </div>
+</nav>
+
+
+<div class="wrapper">
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    include_once 'header.php';
+?>
+
+
+<section class="index-intro">
+    <h1>Home</h1>
+    <p>Home page text</p>
+</section>
+
+<section>
+    <h2>Categories</h2>
+    <div class="index-categories">
+        <div>
+            <h3>Category 1</h3>
+            <p>Category 1 text</p>
+        </div>
+        <div>
+            <h3>Category 2</h3>
+            <p>Category 2 text</p>
+        </div>
+        <div>
+            <h3>Category 3</h3>
+            <p>Category 3 text</p>
+        </div>
+</section>
+
+
+<?php
+include_once 'footer.php';
+?>
